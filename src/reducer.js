5abdf41774cfaa752c1effe3296753645f24344e
r.galkin <galkinrost@gmail.com>
2016-03-08 19:06:56 +0300

    add reducer

---
+++ src/reducer.js
@@ -0,0 +1,73 @@
+import {
+    ADD,
+    BLUR,
+    CHANGE,
+    CHANGE_CARET_POSITION,
+    END_SUBMITTING,
+    END_VALIDATION,
+    FOCUS,
+    INITIALIZE,
+    REMOVE,
+    START_SUBMITTING,
+    START_VALIDATION,
+    VALIDATE
+} from './actionTypes'
+import {List, Map} from 'immutable'
+
+const initialState = Map({
+    fields: Map(),
+    focusedFieldPath: List(),
+    caretPosition: List(),
+    submitting: false
+})
+
+const formReducer = (state = initialState, action = {}) => {
+    switch (action.type) {
+        case INITIALIZE:
+            return state.update(`fields`, fields => fields.merge(action.result.data))
+        case CHANGE:
+            return state.setIn([ `fields`, ...action.result.fieldPath, `value` ], action.result.value)
+        case VALIDATE:
+            return state.setIn([ `fields`, ...action.result.fieldPath, `valid` ], action.result.value)
+        case START_VALIDATION:
+            return state.setIn([ `fields`, ...action.result.fieldPath, `validating` ], true)
+        case END_VALIDATION:
+            return state.updateIn([ `fields`, ...action.result.fieldPath ], field => field.withMutations(f =>
+                f.set(`validating`, false)
+                    .set(`valid`, action.result.value)
+            ))
+        case REMOVE:
+            return state.removeIn([ `fields`, ...action.result.fieldPath, action.result.index ])
+        case ADD:
+            {
+                state = state.updateIn([ `fields`, ...action.result.fieldPath ], (list = List()) =>
+                    typeof action.result.index === `undefined` ? list.push(action.result.data) : list.splice(action.result.index, 0, action.result.data))
+
+                if (action.result.focused) {
+                    state = state.set(`focusedFieldPath`, List([ ...action.result.fieldPath,
+                        typeof action.result.index === `undefined` ? state.getIn([`fields`, ...action.result.fieldPath]).size - 1 : action.result.index]))
+                }
+                return state
+            }
+        case FOCUS:
+            return state.set(`focusedFieldPath`, List(action.result.fieldPath))
+        case CHANGE_CARET_POSITION:
+            return state.withMutations(s =>
+                s.set(`focusedFieldPath`, List(action.result.fieldPath))
+                    .set(`caretPosition`, List(action.result.caretPosition))
+            )
+        case BLUR:
+            return state.withMutations(s =>
+                s.set(`focusedFieldPath`, List())
+                    .set(`caretPosition`, List())
+            )
+        case START_SUBMITTING:
+            return state.set(`submitting`, true)
+        case END_SUBMITTING:
+            return state.set(`submitting`, false)
+    }
+
+    return state
+}
+
+export default formReducer
---
+++ test/reducer.spec.js
@@ -0,0 +1,255 @@
+import * as actionTypes from '../src/actionTypes'
+import {List, Map, fromJS} from 'immutable'
+
+import expect from 'expect'
+import expectImmutable from 'expect-immutable'
+import reducer from '../src/reducer'
+import rndoam from 'rndoam/lib/withImmutable'
+
+expect.extend(expectImmutable)
+
+describe(`redux-uniform`, () => {
+
+    describe(`reducer`, () => {
+
+        it(`should initialize fields`, () => {
+            const data = rndoam.map()
+
+            expect(reducer(fromJS({
+                fields: {}
+            }), {
+                type: actionTypes.INITIALIZE,
+                result: {
+                    data
+                }
+            })).toEqualImmutable(Map({
+                fields: data
+            }))
+        })
+
+        it(`should change field`, () => {
+            expect(reducer(fromJS({
+                fields: {
+                    foo: {
+                        value: `bar`
+                    }
+                }
+            }), {
+                type: actionTypes.CHANGE,
+                result: {
+                    fieldPath: [ `foo` ],
+                    value: `baz`
+                }
+            })).toEqualImmutable(fromJS({
+                fields: {
+                    foo: {
+                        value: `baz`
+                    }
+                }
+            }))
+        })
+
+        it(`should update validity value`, () => {
+            expect(reducer(fromJS({
+                fields: {
+                    foo: {
+                        valid: false
+                    }
+                }
+            }), {
+                type: actionTypes.VALIDATE,
+                result: {
+                    fieldPath: [ `foo` ],
+                    value: true
+                }
+            })).toEqualImmutable(fromJS({
+                fields: {
+                    foo: {
+                        valid: true
+                    }
+                }
+            }))
+        })
+
+        it(`should start validation`, () => {
+
+            expect(reducer(fromJS({
+                fields: {
+                    foo: {
+                        validating: false
+                    }
+                }
+            }), {
+                type: actionTypes.START_VALIDATION,
+                result: {
+                    fieldPath: [ `foo` ]
+                }
+            })).toEqualImmutable(fromJS({
+                fields: {
+                    foo: {
+                        validating: true
+                    }
+                }
+            }))
+        })
+
+        it(`should end validation`, () => {
+            expect(reducer(fromJS({
+                fields: {
+                    foo: {
+                        validating: true,
+                        valid: false
+                    }
+                }
+            }), {
+                type: actionTypes.END_VALIDATION,
+                result: {
+                    fieldPath: [ `foo` ],
+                    value: true
+                }
+            })).toEqualImmutable(fromJS({
+                fields: {
+                    foo: {
+                        validating: false,
+                        valid: true
+                    }
+                }
+            }))
+        })
+
+        it(`should remove item from array`, () => {
+
+            expect(reducer(fromJS({
+                fields: {
+                    foo: [
+                        rndoam.map()
+                    ]
+                }
+            }), {
+                type: actionTypes.REMOVE,
+                result: {
+                    fieldPath: [ `foo` ],
+                    index: 0
+                }
+            })).toEqualImmutable(fromJS({
+                fields: {
+                    foo: []
+                }
+            }))
+        })
+
+        it(`should create empty field in array`, () => {
+            const data = rndoam.map()
+            expect(reducer(fromJS({
+                fields: {
+                    foos: []
+                }
+            }), {
+                type: actionTypes.ADD,
+                result: {
+                    fieldPath: [ `foos` ],
+                    data,
+                    focused: true
+                }
+            })).toEqualImmutable(fromJS({
+                fields: {
+                    foos: [
+                        data
+                    ]
+                },
+                focusedFieldPath: [ `foos` , 0 ]
+            }))
+        })
+
+        it(`should insert value into a specific index`, () => {
+            const item1 = rndoam.map()
+            const item2 = rndoam.map()
+
+            expect(reducer(fromJS({
+                fields: {
+                    foos: [
+                        item1
+                    ]
+                }
+            }), {
+                type: actionTypes.ADD,
+                result: {
+                    fieldPath: [ `foos` ],
+                    data: item2,
+                    index: 0,
+                    focused: true
+                }
+            })).toEqualImmutable(fromJS({
+                fields: {
+                    foos: [
+                        item2,
+                        item1
+                    ]
+                },
+                focusedFieldPath: [ `foos`, 0 ]
+            }))
+        })
+
+        it(`should update focusFieldPath`, () => {
+
+            expect(reducer(fromJS({
+                focusedFieldPath: []
+            }), {
+                type: actionTypes.FOCUS,
+                result: {
+                    fieldPath: [ `foo` ]
+                }
+            })).toEqualImmutable(fromJS({
+                focusedFieldPath: [ `foo` ]
+            }))
+        })
+
+        it(`should update caretPosition field`, () => {
+            expect(reducer(fromJS({
+                focusedFieldPath: [],
+                caretPosition: []
+            }), {
+                type: actionTypes.CHANGE_CARET_POSITION,
+                result: {
+                    fieldPath: [ `foo` ],
+                    caretPosition: [ 0, 1 ]
+                }
+            })).toEqualImmutable(fromJS({
+                focusedFieldPath: [ `foo` ],
+                caretPosition: [ 0, 1 ]
+            }))
+        })
+
+        it(`should clear fields of focused state on blur`, () => {
+            expect(reducer(fromJS({
+                focusedFieldPath: rndoam.array(),
+                caretPosition: rndoam.array()
+            }), {
+                type: actionTypes.BLUR
+            })).toEqualImmutable(fromJS({
+                focusedFieldPath: [],
+                caretPosition: []
+            }))
+        })
+
+        it(`should set submitting true`, () => {
+            expect(reducer(fromJS({
+                submitting: false
+            }), {
+                type: actionTypes.START_SUBMITTING
+            })).toEqualImmutable(fromJS({
+                submitting: true
+            }))
+        })
+
+        it(`should set submitting false`, () => {
+            expect(reducer(fromJS({
+                submitting: true
+            }), {
+                type: actionTypes.END_SUBMITTING
+            })).toEqualImmutable(fromJS({
+                submitting: false
+            }))
+        })
+    })
+})
